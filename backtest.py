bed76436cd6c74a52cc5824895e9b1ce4caa4562
varunraisinghal <varun.singhalacademic@yahoo.com>
2023-02-09 13:03:24 -0800

    All data sets

--- backtest.py
+++ backtest.py
@@ -2,15 +2,35 @@ import csv
 import pandas as pd
 import numpy as np
 
-POINT_SPREAD = 6.5
-POINT_FACTOR = 1.5
+POINT_SPREAD = 7.0
+POINT_FACTOR = 2.0
 QUARTERS = 2
 SECOND_HALF_THRESHOLD = -1.5
 
-df1 = pd.read_csv("data/FantasyOdds.2022/Game.2023.csv")
-df2 = pd.read_csv("data/FantasyOdds.2022/Quarter.2023.csv")
+game1 = pd.read_csv("data/FantasyOdds.2022/Game.2020.csv")
+game2 = pd.read_csv("data/FantasyOdds.2022/Game.2021.csv")
+game3 = pd.read_csv("data/FantasyOdds.2022/Game.2022.csv")
+game4 = pd.read_csv("data/FantasyOdds.2022/Game.2023.csv")
+# gameMerge = pd.merge(game1, game2)
+# gameMerge = pd.merge(gameMerge, game3)
+# gameMerge = pd.merge(gameMerge, game4)
 
-merged_df = pd.merge(df1, df2, on='GameID')
+quarter1 = pd.read_csv("data/FantasyOdds.2022/Quarter.2020.csv")
+quarter2 = pd.read_csv("data/FantasyOdds.2022/Quarter.2021.csv")
+quarter3 = pd.read_csv("data/FantasyOdds.2022/Quarter.2022.csv")
+quarter4 = pd.read_csv("data/FantasyOdds.2022/Quarter.2023.csv")
+# quarterMerge = pd.merge(quarter1, quarter2)
+# quarterMerge = pd.merge(quarterMerge, quarter3)
+# quarterMerge = pd.merge(quarterMerge, quarter4)
+
+
+merged_df = pd.merge(game4, quarter4, on='GameID')
+
+# Filter rows based on the date being after 1/01/2023 12:00:00 AM
+# filter_date = pd.Timestamp("1/1/2023 00:00:00")
+# merged_df["Day"] = pd.to_datetime(merged_df["Day"])
+# merged_df = merged_df[merged_df["Day"] >= filter_date]
+print(merged_df)
 
 merged_df = merged_df[merged_df['Number'] <= QUARTERS]
 
@@ -51,7 +71,7 @@ print("Away Team Favored")
 # print(grouped_df_home['second half diff'])
 
 
-print("2H Comeback Mean", grouped_df_home['2H Home Team up'].describe())
+print(grouped_df_home['2H Home Team up'].describe())
 #print("Away Mean", grouped_df_away['Away Team halftime up'].mean())
 
 #print("Home 2H Median", grouped_df_home['2H Home Team up'].median())
@@ -64,6 +84,7 @@ print("2H Comeback Mean", grouped_df_home['2H Home Team up'].describe())
 home_win =  len(grouped_df_home[grouped_df_home['2H Home Team up'] > SECOND_HALF_THRESHOLD]) / len(grouped_df_home.index)
 
 print("Home Percentage Win", home_win)
+print("Number of points", len(grouped_df_home[grouped_df_home['2H Home Team up'] > SECOND_HALF_THRESHOLD]))
 
 
 # GameID = []
